9847ddd28147baa4e11a6c8b60c82bcf6fa3ef67
tispffldk27 <tispffldk27@naver.com>
2019-08-05 11:18:21 +0900

    Mybatis 연동 및 테스트 완료

--- src/main/java/kr/green/test/HomeController.java
+++ src/main/java/kr/green/test/HomeController.java
@@ -1,17 +1,24 @@
 package kr.green.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import kr.green.test.service.MemberService;
+import kr.green.test.vo.MemberVO;
+
 
 @Controller
 public class HomeController {
-	
+	@Autowired
+	MemberService memberService;
 	
 	@RequestMapping(value= "/")
 	public ModelAndView openTilesView(ModelAndView mv) throws Exception{
-	    mv.setViewName("/main/home");
+	    MemberVO mVo = memberService.getMember("a123123123");
+	    System.out.println(mVo);
+		mv.setViewName("/main/home");
 	    mv.addObject("setHeader", "타일즈");
 	    return mv;
 	}
